fc1949d20a2f3a05f056800452c0b230bacd78eb
Anil Kulkarni <anil@textio.com>
2016-07-14 14:59:06 -0700

    Fix package to work with current versions of karma

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ var createPattern = function(path) {
 };
 
 var initReporter = function(files,  baseReporterDecorator) {
-
   baseReporterDecorator(this);
 
-  files.unshift(createPattern(__dirname + '/lib/adapter.js'));
-  files.unshift(createPattern(__dirname + '/lib/html.jasmine.reporter.js'));
-  files.unshift(createPattern(__dirname + '/css/jasmine.css'));
+  // From https://github.com/taras42/karma-jasmine-html-reporter/issues/4#issuecomment-230168638
+  var TEST_JASMINE_CORE = /(\/karma-jasmine\/)/i;
+  var jasmineIndex = 0;
+
+  files.forEach(function(file, index) {
+    if (TEST_JASMINE_CORE.test(file.pattern)) {
+      jasmineIndex = index;
+    }
+  });
+
+  files.splice(++jasmineIndex, 0, createPattern(__dirname + '/css/jasmine.css'));
+  files.splice(++jasmineIndex, 0, createPattern(__dirname + '/lib/html.jasmine.reporter.js'));
+  files.splice(++jasmineIndex, 0, createPattern(__dirname + '/lib/adapter.js'));
 };
 
 initReporter.$inject = ['config.files',  'baseReporterDecorator'];
